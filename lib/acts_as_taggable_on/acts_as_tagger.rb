29bedfe65212841386c76ea68eae0b43229ba458
Mathieu Fosse <mathieu.fosse@gmail.com>
2008-07-24 20:11:26 +0200

    Added options to tag_count method for tagger

--- lib/acts_as_taggable_on/acts_as_tagger.rb
+++ lib/acts_as_taggable_on/acts_as_tagger.rb
@@ -41,7 +41,7 @@ module ActiveRecord
         end
         
         # Return all tags tagged by tagger with count
-        def tag_counts
+        def tag_counts(options={})
           joins = ["LEFT OUTER JOIN #{Tagging.table_name} ON #{Tag.table_name}.id = #{Tagging.table_name}.tag_id"]
           conditions = "#{Tagging.table_name}.tagger_type = '#{self.class}' AND #{Tagging.table_name}.tagger_id = #{self.id}"
           group_by  = "#{Tag.table_name}.id, #{Tag.table_name}.name HAVING COUNT(*) > 0"
@@ -51,7 +51,7 @@ module ActiveRecord
             :conditions => conditions,
             :joins      => joins.join(" "),
             :group      => group_by
-          })
+          }.merge(options))
         end
       end
       
