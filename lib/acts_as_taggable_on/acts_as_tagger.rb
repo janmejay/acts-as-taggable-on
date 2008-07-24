c31a98c79b6eb38dc2358f308be2bf19f850c8fa
Mathieu Fosse <mathieu.fosse@gmail.com>
2008-07-24 18:35:32 +0200

    Oops add missed tagger_id

--- lib/acts_as_taggable_on/acts_as_tagger.rb
+++ lib/acts_as_taggable_on/acts_as_tagger.rb
@@ -43,7 +43,7 @@ module ActiveRecord
         # Return all tags tagged by tagger with count
         def tag_counts
           joins = ["LEFT OUTER JOIN #{Tagging.table_name} ON #{Tag.table_name}.id = #{Tagging.table_name}.tag_id"]
-          conditions = "taggings.tagger_type = '#{self.class}'"
+          conditions = "#{Tagging.table_name}.tagger_type = '#{self.class}' AND #{Tagging.table_name}.tagger_id = #{self.id}"
           group_by  = "#{Tag.table_name}.id, #{Tag.table_name}.name HAVING COUNT(*) > 0"
           
           Tag.find(:all, {
