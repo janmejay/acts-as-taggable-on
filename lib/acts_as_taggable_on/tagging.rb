0bddc35ff0b568779c198e9233fd49ca228b320f
David Masover <dave@Geordi.local>
2008-09-24 17:36:08 -0500

    Added 'only_tags' and 'except_tags' named scopes to Taggings, and bumped gem version.
    This was written for a very specific reason: I needed banned tags, so I needed a way to destroy a bunch of tags, regardless of owner. The tag_list wasn't doing it. Not the cleanest solution, but it lets me do this:
    
    foo.tag_taggables.with_tags(foo.banned_tags).destroy_all

--- lib/acts_as_taggable_on/tagging.rb
+++ lib/acts_as_taggable_on/tagging.rb
@@ -3,4 +3,13 @@ class Tagging < ActiveRecord::Base #:nodoc:
   belongs_to :taggable, :polymorphic => true
   belongs_to :tagger, :polymorphic => true
   validates_presence_of :context
+  
+  # Be careful with these -- they sanitize nothing.
+  # Pass them actual tags or tag ids.
+  named_scope :only_tags, lambda {|tags|
+    {:conditions => ['taggings.tag_id IN (?)', tags]}
+  }
+  named_scope :except_tags, lambda {|tags|
+    {:conditions => ['taggings.tag_id NOT IN (?)', tags]}
+  }
 end
