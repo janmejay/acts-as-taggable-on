ad04845cbc600d0308c218ed582af0a475a34515
David Masover <dave@3mix.com>
2008-11-07 15:15:34 -0600

    Added with_context and except_context scopes.
    Also, quoted the table name throughout this file.

--- lib/acts_as_taggable_on/tagging.rb
+++ lib/acts_as_taggable_on/tagging.rb
@@ -4,12 +4,19 @@ class Tagging < ActiveRecord::Base #:nodoc:
   belongs_to :tagger, :polymorphic => true
   validates_presence_of :context
   
+  named_scope :with_context, lambda {|context|
+    {:conditions => ["#{quoted_table_name}.context = ?", context]}
+  }
+  named_scope :except_context, lambda {|context|
+    {:conditions => ["#{quoted_table_name}.context <> ?", context]}
+  }
+  
   # Be careful with these -- they sanitize nothing.
   # Pass them actual tags or tag ids.
   named_scope :only_tags, lambda {|tags|
-    {:conditions => ['taggings.tag_id IN (?)', tags]}
+    {:conditions => ["#{quoted_table_name}.tag_id IN (?)", tags]}
   }
   named_scope :except_tags, lambda {|tags|
-    {:conditions => ['taggings.tag_id NOT IN (?)', tags]}
+    {:conditions => ["#{quoted_table_name}.tag_id NOT IN (?)", tags]}
   }
 end
